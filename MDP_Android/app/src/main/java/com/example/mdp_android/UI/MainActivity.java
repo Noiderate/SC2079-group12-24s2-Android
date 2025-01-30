526107e82cf94c547c32a12744aaf55a06131c29
Noiderate <60457778+Noiderate@users.noreply.github.com>
2025-01-30 17:52:28 +0800

    base android file
    practicing how to use bluetooth

---
+++ MDP_Android/app/src/main/java/com/example/mdp_android/UI/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.mdp_android.UI;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mdp_android.R;
+import com.example.mdp_android.controller.BluetoothController;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> requestBluetooth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        requestBluetooth = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(androidx.activity.result.ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            // Bluetooth was granted
+                            Toast.makeText(MainActivity.this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Bluetooth was denied
+                            Toast.makeText(MainActivity.this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+
+        Button bluetoothButton = findViewById(R.id.bluetoothButton);
+
+        bluetoothButton.setOnClickListener(v -> {
+
+            Log.d("bluetooth1", "bluetooth button was clicked, we gonna change to a new screen now");
+            // im gonna fking kms do i just suck at coding or what
+            BluetoothController bluetoothController = new BluetoothController(this, requestBluetooth);
+            bluetoothController.getBluetoothAdapter();
+            boolean isBluetoothEnabled = bluetoothController.enableBluetooth();
+
+            if (isBluetoothEnabled) {
+                Intent openBluetoothConnectionActivity = new Intent(this, BluetoothConnectionActivity.class);
+                startActivity(openBluetoothConnectionActivity);
+            }
+        });
+    }
+}
---
+++ MDP_Android/app/src/main/java/com/example/mdp_android/controller/BluetoothController.java
@@ -0,0 +1,81 @@
+package com.example.mdp_android.controller;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.mdp_android.UI.MainActivity;
+
+public class BluetoothController {
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private Context context;
+    private BluetoothAdapter blAdapter;
+    ActivityResultLauncher<Intent> bluetoothLauncher;
+    public BluetoothController(Context context, ActivityResultLauncher<Intent> bluetoothLauncher) {
+        this.context = context;
+        this.bluetoothLauncher = bluetoothLauncher;
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            {
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
+                return;
+            }
+        }
+    }
+
+    public BluetoothAdapter getBluetoothAdapter() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Bluetooth is not supported on this device, show a toast
+            Toast.makeText(context, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        blAdapter = bluetoothAdapter;
+        Log.d("bluetooth1", "bluetooth is supported on this device");
+        return bluetoothAdapter;
+    }
+
+
+    public boolean enableBluetooth() {
+
+        if (!blAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            bluetoothLauncher.launch(enableBtIntent);
+            Toast.makeText(context, "Please enable bluetooth and try again", Toast.LENGTH_SHORT).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
+//        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            Log.d("bluetooth1","checking permissions");
+//            ActivityCompat.requestPermissions((Activity) context,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSION_REQUEST_CODE);
+//        }
+//
+//        if (!blAdapter.isEnabled()) {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            if (context instanceof Activity) {
+//                ((Activity) context).startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//            }
+//            Log.d("bluetooth1","enable bluetooth");
+//        } else {
+//            Toast.makeText(context, "Bluetooth is already enabled", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+}
